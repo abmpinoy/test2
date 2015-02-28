612161dc5b776f58342d15ffdcf4e8e1df220378
amora <abmpinoy@gmail.com>
2015-02-28 10:09:29 -0500

    remoted

--- host.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import subprocess as sub
-
-x = range(33,63)
-for num in x:
-    ip = '69.28.20.' + str(num)
-    print sub.Popen(('host',ip), stdout=sub.PIPE).stdout.read(),
