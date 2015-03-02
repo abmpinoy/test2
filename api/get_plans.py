eadeb3192f64311e9549eeafdcd015cdd314ebc6
amora <abmpinoy@gmail.com>
2015-03-02 12:20:15 -0500

    Converted for centralized variables

--- api/get_plans.py
+++ api/get_plans.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python
 
-import json, random, hashlib, string, time, hmac, base64
+from vars import *
 import requests as req
 
-def id_generator(size=20, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-urlbase = "https://cloudapi.atlantic.net/api.php/?Action=describe-plan&Version=2010-12-30"
-ACSAccessKeyId = "ATL981de504cb0f5c4528d959abe0fd465b"
-priv_key = "5408e3d4de33c7adfdbf72ed648bddc80609890a"
-Format = "json"
-Timestamp= int(float(time.time()))
-Rndguid = id_generator()
-string_to_sign = str(Timestamp) + Rndguid
-message = bytes(string_to_sign).encode('utf-8')
-secret = bytes(priv_key).encode('utf-8')
-Signature = base64.b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())
-
+# Set method
+Action = "describe-plan"
 
 # Assemble query
-r = urlbase + "&ACSAccessKeyId=" + ACSAccessKeyId + "&Format=" + Format \
+r = urlbase + "Action=" + Action + "&Version=" + Version + "&ACSAccessKeyId=" + ACSAccessKeyId + "&Format=" + Format \
 + "&Timestamp=" + str(Timestamp) + "&Rndguid=" + Rndguid + "&Signature=" + Signature
 print "\nQuery:"
 print r + "\n"
