d40ea5954f719d09ca71f563667b69ee9ce96511
amora <abmpinoy@gmail.com>
2015-03-02 09:52:06 -0500

    Made executable

--- api/list_cloud_servers.py
+++ api/list_cloud_servers.py
@@ -6,7 +6,7 @@ import requests as req
 def id_generator(size=20, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-urlbase = "https://cloudapi.atlantic.net/api.php/?Action=run-instance&Version=2010-12-30"
+urlbase = "https://cloudapi.atlantic.net/api.php/?Action=list-instances&Version=2010-12-30"
 ACSAccessKeyId = "ATL981de504cb0f5c4528d959abe0fd465b"
 priv_key = "5408e3d4de33c7adfdbf72ed648bddc80609890a"
 Format = "json"
@@ -16,16 +16,11 @@ string_to_sign = str(Timestamp) + Rndguid
 message = bytes(string_to_sign).encode('utf-8')
 secret = bytes(priv_key).encode('utf-8')
 Signature = base64.b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())
-planname =  raw_input("Pick an option(S,M,L,XL,XXL: ")
-servername = raw_input("Server Name: ")
-#planname = "L"
-#servername = "coding_test"
-imageid = "CentOS-6.5_64bit"
+#Signature = hashlib.sha256(message + secret).digest().encode("base64")
 
 # Assemble query
 r = urlbase + "&ACSAccessKeyId=" + ACSAccessKeyId + "&Format=" + Format \
-+ "&Timestamp=" + str(Timestamp) + "&Rndguid=" + Rndguid + "&Signature=" + Signature + "&servername=" + servername \
-+ "&imageid=" + imageid + "&planname=" + planname.upper()
++ "&Timestamp=" + str(Timestamp) + "&Rndguid=" + Rndguid + "&Signature=" + Signature
 print "\nQuery:"
 print r + "\n"
 
@@ -37,9 +32,22 @@ print "message: " + message
 print "secret: " + secret
 print "Signature: " + Signature + "\n"
 
-
 # Pull data
 data = req.get(r).json()
 print data
-#items =  data['describe-imageresponse']['imagesset']
+items =  data['list-instancesresponse']['instancesSet']
 
+# Count VMs and output their data
+count = 0
+for item in items:
+    vmdata = items[item]
+    print "Name: " + vmdata['vm_description']
+    print "OS: " + vmdata['vm_image']
+    print "Instance ID: " + vmdata['InstanceId']
+    print "IP: " + vmdata['vm_ip_address']
+    print "RAM: " + vmdata['vm_ram_req']
+    print "CPU Cores: " + vmdata['vm_cpu_req']
+    print "Disk Size: " + vmdata['vm_disk_req']
+    print "Created on: " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(vmdata['vm_created_date']))) + "\n"
+    count+=1
+print str(count) + " Virtual Machines detected"
