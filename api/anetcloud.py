9e247de6844cc7e47be33e3422fa40d728dc4bf1
amora <abmpinoy@gmail.com>
2015-03-01 23:08:25 -0500

    Full function api

--- api/anetcloud.py
+++ api/anetcloud.py
@@ -12,24 +12,32 @@ priv_key = "5408e3d4de33c7adfdbf72ed648bddc80609890a"
 Format = "json"
 Timestamp= int(float(time.time()))
 Rndguid = id_generator()
-
 string_to_sign = str(Timestamp) + Rndguid
 message = bytes(string_to_sign).encode('utf-8')
 secret = bytes(priv_key).encode('utf-8')
 Signature = base64.b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())
+#Signature = hashlib.sha256(message + secret).digest().encode("base64")
 
-# Build query
+# Assemble query
 r = urlbase + "&ACSAccessKeyId=" + ACSAccessKeyId + "&Format=" + Format \
 + "&Timestamp=" + str(Timestamp) + "&Rndguid=" + Rndguid + "&Signature=" + Signature
-print r
-print
+print "\nQuery:"
+print r + "\n"
 
+print "Debug information: "
+print "Timestamp: " + str(Timestamp)
+print "Rndguid: " + Rndguid
+print "string_to_sign: " + string_to_sign
+print "message: " + message
+print "secret: " + secret
+print "Signature: " + Signature + "\n"
+
+# Pull data
 data = req.get(r).json()
 print data
+items =  data['list-instancesresponse']['instancesSet']
 
-inst = data['list-instancesresponse']
-items = inst['instancesSet']
-
+# Count VMs and output their data
 count = 0
 for item in items:
     vmdata = items[item]
@@ -40,8 +48,6 @@ for item in items:
     print "RAM: " + vmdata['vm_ram_req']
     print "CPU Cores: " + vmdata['vm_cpu_req']
     print "Disk Size: " + vmdata['vm_disk_req']
-    print "Created on: " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(vmdata['vm_created_date'])))
-    print 
+    print "Created on: " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(vmdata['vm_created_date']))) + "\n"
     count+=1
 print str(count) + " Virtual Machines detected"
-
